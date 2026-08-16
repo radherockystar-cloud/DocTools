f7490cbeaf98d500578afc4bae015cf84b6ebd22
radherockystar-cloud <radherockystar@gmail.com>
2026-08-16 17:52:33 +0530

    Add Service Worker and update manifest

---
+++ public/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'doctools-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request).catch(() => caches.match('/')))
+  );
+});
